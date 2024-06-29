cb4f25cb878d72335ceaf0c33d43f84ad168f374
Boodenator <boode1000@gmail.com>
2024-06-29 15:23:35 -0700

    task6

--- models/base_model.py
+++ models/base_model.py
@@ -1,108 +1,82 @@
 #!/usr/bin/python3
-"""
-The BaseModel will act as base class of
-all models in project."""
-
+"""AirBnB base model class"""
+from sqlalchemy.ext.declarative import declarative_base
 import models
-from uuid import uuid4
+import uuid
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+
+
+Base = declarative_base()
 
 
 class BaseModel:
+    """attributes/methods class definition of other classes
+    """
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
 
     def __init__(self, *args, **kwargs):
-        """The Constructor to deserialize or initialize a class"""
-
-        # initialization
-        if kwargs == {} or None:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+        """base model class instantiation
+        Argumnts:
+            args: IGNORED
+            kwargs: constructor of the BaseModel argumnts
+        Attributes:
+            id: genertaing unique id
+            created_at: date creation
+            updated_at: date updating
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
         else:
-
-        # deserialization
-            for key, val in kwargs.items():
-                if key == '__class__':
-                    continue
-                self.__dict__[key] = val
-            if 'created_at' in kwargs:
-                self.created_at = datetime.strptime(
-                    kwargs['created_at'],
-                    '%Y-%m-%dT%H:%M:%S.%f')
-            if 'updated_at' in kwargs:
-                self.updated_at = datetime.strptime(
-                    kwargs['updated_at'],
-                    '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                self.__dict__[key] = kwargs[key]
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """bypass self representation of str"""
-        fmt = "[{}] ({}) {}"
-        return fmt.format(
-                type(self).__name__,
-                self.id,
-                self.__dict__)
+        """returns a string
+        Return:
+            returns a string of class name, id, and dictionary
+        """
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+
+    def __repr__(self):
+        """string representaion return
+        """
+        return self.__str__()
 
     def save(self):
-        """updates variables"""
-        self.updated_at = datetime.utcnow()
+        """public instance attribute updated into current
+        """
+        self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary representation of self"""
-        temp = {**self.__dict__}
-        temp['__class__'] = type(self).__name__
-        temp['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        temp['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        return temp
+        """dictionary creation
+        Return:
+            returning dict for all key values
+        """
+        my_dict = dict(self.__dict__)
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        if '_sa_instance_state' in my_dict.keys():
+            del my_dict['_sa_instance_state']
+        return my_dict
 
-    @classmethod
-    def all(cls):
-        """Return every current instances of cls"""
-        return models.storage.find_all(cls.__name__)
-
-    @classmethod
-    def count(cls):
-        """collect numbers of current instances of cls"""
-        return len(models.storage.find_all(cls.__name__))
-
-    @classmethod
-    def create(cls, *args, **kwargs):
-        """Instance creation"""
-        new = cls(*args, **kwargs)
-        return new.id
-
-    @classmethod
-    def show(cls, instance_id):
-        """Instance retrival"""
-        return models.storage.find_by_id(
-            cls.__name__,
-            instance_id
-        )
-
-    @classmethod
-    def destroy(cls, instance_id):
-        """Instance deletion"""
-        return models.storage.delete_by_id(
-            cls.__name__,
-            instance_id
-        )
-
-    @classmethod
-    def update(cls, instance_id, *args):
-        """Instance update"""
-        if not len(args):
-            print("** attribute name missing **")
-            return
-        if len(args) == 1 and isinstance(args[0], dict):
-            args = args[0].items()
-        else:
-            args = [args[:2]]
-        for arg in args:
-            models.storage.update_one(
-                cls.__name__,
-                instance_id,
-                *arg
-            )
+    def delete(self):
+        """ Object deletion
+        """
+        models.storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-""" City  class"""
+"""city class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    """city class"""
-
-    state_id: str = ""
-    name: str = ""
+class City(BaseModel, Base):
+    """specific class for City
+    Attributes:
+        state_id: The state name
+        name: name
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
