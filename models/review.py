fcb18509a94f63146e33ad6aad7fbc31433a0a3a
Boodenator <boode1000@gmail.com>
2024-06-29 15:43:00 -0700

    task fx

--- models/review.py
+++ models/review.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-""" Review  class"""
+"""review class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
-from models.base_model import BaseModel
-
-
-class Review(BaseModel):
-    """Reviewclass"""
-    place_id: str = ""
-    user_id: str = ""
-    text: str = ""
+class Review(BaseModel, Base):
+    """new class for Review
+    Attributes:
+        place_id: place name
+        user_id: user name
+        text: describe review
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
